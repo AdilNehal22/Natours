68df130eb602aec28f2dae87c0374ced73833839
AdilNehal22 <adilnehal80@gmail.com>
2021-10-17 22:17:12 +0500

    Implemented CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,10 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
+//Implementing CORS
+app.use(cors());
+app.options('*', cors())
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
